f06a723a54d8c210df826fff3d208c9505d6ed45
Crie <crosssceneofwindff@gmail.com>
2018-07-20 01:00:14 +0900

    update

--- srresnet/model.py
+++ srresnet/model.py
@@ -4,19 +4,31 @@ import chainer.functions as F
 from chainer import Chain, cuda, Variable
 import numpy as np
 
+def pixel_shuffler(out_ch, x, r = 2):
+    b, c, w, h = x.shape
+    x = x.reshape(b, r, r, int(out_ch/(r*2)), w, h)
+    x = x.transpose(0,3,4,1,5,2)
+    out_map = x.reshape(b, int(out_ch/(r*2)), w*r, h*r)
+
+    return out_map
+
 class CBR(Chain):
     def __init__(self, in_ch, out_ch, bn = True, activation = F.relu):
-        super.__init__(CBR, self)
+        super(CBR,self).__init__()
         self.activation = activation
+        self.bn = bn
+        self.out_ch = out_ch
         w = chainer.initializers.Normal(0.02)
-        with init_scope():
-            self.c0 = L.Deconvolution2D(in_ch, out_ch, 3,1,1, initialW = w)
+        with self.init_scope():
+            self.c0 = L.Deconvolution2D(in_ch, out_ch, 4,2,1, initialW = w)
 
-            self.bn0 = L.BatchNormalization(out_ch)
+            self.bn0 = L.BatchNormalization(int(out_ch))
 
     def __call__(self,x):
         h = self.c0(x)
-        if bn:
+        #h = pixel_shuffler(self.out_ch, h)
+
+        if self.bn:
             h = self.bn0(h)
         if self.activation is not None:
             h = self.activation(h)
@@ -25,9 +37,9 @@ class CBR(Chain):
 
 class Gen_ResBlock(Chain):
     def __init__(self, in_ch, hid_ch):
-        super.__init__(Gen_ResBlock,self)
+        super(Gen_ResBlock, self).__init__()
         w = chainer.initializers.Normal(0.02)
-        with init_scope():
+        with self.init_scope():
             self.c0 = L.Deconvolution2D(in_ch, hid_ch,3,1,1, initialW = w)
             self.c1 = L.Deconvolution2D(hid_ch, hid_ch,3,1,1, initialW = w)
 
@@ -42,9 +54,9 @@ class Gen_ResBlock(Chain):
 
 class Dis_ResBlock(Chain):
     def __init__(self, in_ch, out_ch):
-        super.__init__(Dis_ResBlock,self)
+        super(Dis_ResBlock, self).__init__()
         w = chainer.initializers.Normal(0.02)
-        with init_scope():
+        with self.init_scope():
             self.c0 = L.Convolution2D(in_ch, out_ch, 3,1,1, initialW = w)
             self.c1 = L.Convolution2D(out_ch, out_ch, 3,1,1, initialW = w)
     
@@ -59,41 +71,39 @@ class Dis_ResBlock(Chain):
 class Generator(Chain):
     def __init__(self, base = 64):
 
-        super.__init__(Generator, self)
+        super(Generator, self).__init__()
         w = chainer.initializers.Normal(0.02)
-        with init_scope():
+        with self.init_scope():
 
-            self.l0 = L.Linear(100, 64*16*116, initialW = w)
+            self.l0 = L.Linear(100, 64*16*16, initialW = w)
             self.r0 = Gen_ResBlock(base, base)
             self.r1 = Gen_ResBlock(base, base)
             self.r2 = Gen_ResBlock(base, base)
             self.c0 = L.Deconvolution2D(base,base,3,1,1,initialW = w)
             self.cbr0 = CBR(base, base*4)
-            self.cbr1 = CBR(base*4, base*4)
-            self.cbr2 = CBR(base*4, base*4)
-            self.c1 = L.Deconvolution2D(base*4, 3, 9, 1, 1, initialW = w)
+            self.cbr1 = CBR(base*4, base)
+            self.c1 = L.Convolution2D(base, 3, 3, 1, 1, initialW = w)
 
             self.bn0 = L.BatchNormalization(64*16*16)
             self.bn1 = L.BatchNormalization(base)
 
     def __call__(self,x):
-        h1 = F.relu(self.bn0(self.l0(x)))
+        h1 = F.reshape(F.relu(self.bn0(self.l0(x))), (100, 64, 16,16))
         h = self.r0(h1)
         h = self.r1(h)
         h = self.r2(h)
         h = h1 + F.relu(self.bn1(self.c0(h)))
         h = self.cbr0(h)
         h = self.cbr1(h)
-        h = self.cbr2(h)
         h = F.tanh(self.c1(h))
 
         return h
 
 class Discriminator(Chain):
     def __init__(self,base = 32):
-        super.__init__(Discriminator, self)
+        super(Discriminator, self).__init__()
         w = chainer.initializers.Normal(0.02)
-        with init_scope():
+        with self.init_scope():
             self.c0 = L.Convolution2D(3,base,4,2,1,initialW = w)
             self.r0 = Dis_ResBlock(base,base)
             self.r1 = Dis_ResBlock(base,base)
@@ -104,10 +114,10 @@ class Discriminator(Chain):
             self.r4 = Dis_ResBlock(base*4,base*4)
             self.r5 = Dis_ResBlock(base*4,base*4)
             self.c3 = L.Convolution2D(base*4, base*8, 4,2,1,initialW = w)
-            self.r6 = Dis_ResBlock(base*8, base*8)
-            self.r7 = Dis_ResBlock(base*8, base*8)
-            self.c4 = L.Convolution2D(base*8, base*16, 4,2,1,initialW = w)
-            self.l0 = L.Linear(base*16,1,initialW = w)
+           # self.r6 = Dis_ResBlock(base*8, base*8)
+           # self.r7 = Dis_ResBlock(base*8, base*8)
+            #self.c4 = L.Convolution2D(base*8, base*16, 4,2,1,initialW = w)
+            self.l0 = L.Linear(256*4*4,1,initialW = w)
     
     def __call__(self,x):
         h = F.leaky_relu(self.c0(x))
@@ -120,11 +130,9 @@ class Discriminator(Chain):
         h = self.r4(h)
         h = self.r5(h)
         h = F.leaky_relu(self.c3(h))
-        h = self.r5(h)
-        h = self.r6(h)
-        h = F.leaky_relu(self.c4(h))
-        h = self.h0(h)
+        #h = self.r6(h)
+        #h = self.r7(h)
+        #h = F.leaky_relu(self.c4(h))
+        h = self.l0(h)
 
         return h
-        
-        
--- srresnet/train.py
+++ srresnet/train.py
@@ -1,14 +1,16 @@
 import chainer
 import chainer.links as L
 import chainer.functions as F
-from chainer import cuda, Chain, Variable, serializers, optimizers]
+from chainer import cuda, Chain, Variable, serializers, optimizers
 import numpy as np
 import os
 import pylab
 import argparse
-from model import Generator, Discriminator]
+from model import Generator, Discriminator
+import math
 
 xp = cuda.cupy
+cuda.get_device(0).use()
 
 def set_optimizer(model, alpha=0.002, beta=0.5):
     optimizer = optimizers.Adam(alpha = alpha, beta1 = beta)
@@ -16,17 +18,33 @@ def set_optimizer(model, alpha=0.002, beta=0.5):
 
     return optimizer
 
+def loss_func_dcgan_dis_real(h):
+    return F.sum(F.softplus(-h)) / batchsize
+
+def loss_func_dcgan_dis_fake(h):
+    return F.sum(F.softplus(h)) / batchsize
+
 parser = argparse.ArgumentParser(description="SRResNet")
 parser.add_argument("--epoch", default=1000, type=int, help="the number of epochs")
 parser.add_argument("--batchsize", default=100, type=int, help="batch size")
 parser.add_argument("--interval", default=10, type=int, help="the interval of snapshot")
 parser.add_argument("--lam1", default=10.0, type=float, help="the weight of gradient penalty")
+parser.add_argument("--type", default = None, help ="select Normal or RaGAN")
 
 args = parser.parse_args()
 epochs = args.epoch
 batchsize = args.batchsize
 interval = args.interval
 lambda1 = args.lam1
+gan_type = args.type
+wid = int(math.sqrt(batchsize))
+
+x_train = np.load("../DCGAN/face_getchu.npy").astype(np.float32)
+Ntrain, channels, width, height = x_train.shape
+
+image_dir = "./output/"
+if not os.path.exists(image_dir):
+    os.mkdir(image_dir)
 
 generator = Generator()
 generator.to_gpu()
@@ -35,3 +53,72 @@ gen_opt = set_optimizer(generator)
 discriminator = Discriminator()
 discriminator.to_gpu()
 dis_opt = set_optimizer(discriminator)
+
+zvis = xp.random.uniform(-1,1,(batchsize,100),dtype=xp.float32)
+
+for epoch in range(epochs):
+    sum_dis_loss = np.float32(0)
+    sum_gen_loss = np.float32(0)
+    for batch in range(0,Ntrain,batchsize):
+        x_dis = np.zeros((batchsize,channels,width,height), dtype=np.float32)
+        for j in range(batchsize):
+            rnd = np.random.randint(Ntrain)
+            x_dis[j,:,:,:] = x_train[rnd]
+
+        x_dis = cuda.to_gpu(x_dis)
+
+        z = Variable(xp.random.uniform(-1,1,(batchsize,100), dtype = xp.float32))
+        x_fake = generator(z)
+        y_fake = discriminator(x_fake)
+
+        std_data = xp.std(x_dis, axis=0, keepdims = True)
+        rnd_x = xp.random.uniform(0,1,x_dis.shape).astype(xp.float32)
+        x_perturbed = Variable(cuda.to_gpu(x_dis + 0.5*rnd_x*std_data))
+
+        x_real = Variable(x_dis)
+        y_dis = discriminator(x_real)
+        y_perturbed = discriminator(x_perturbed)
+        grad, = chainer.grad([y_perturbed],[x_perturbed], enable_double_backprop=True)
+        grad = F.sqrt(F.batch_l2_norm_squared(grad))
+        loss_grad = lambda1 * F.mean_squared_error(grad, xp.ones_like(grad.data))
+
+        if gan_type == "Normal":
+            loss_dis = loss_func_dcgan_dis_real(y_dis) + loss_func_dcgan_dis_fake(y_fake) + loss_grad
+            loss_gen = loss_func_dcgan_dis_real(y_fake)
+
+        if gan_type == "RaGAN":
+            fake_mean = F.broadcast_to(F.mean(y_fake), (batchsize,1))
+            real_mean = F.broadcast_to(F.mean(y_dis), (batchsize,1))
+
+            loss_dis = (loss_func_dcgan_dis_real(y_dis - fake_mean) + loss_func_dcgan_dis_fake(y_fake - real_mean))/2 + loss_grad
+            loss_gen = (loss_func_dcgan_dis_real(y_fake - real_mean) + loss_func_dcgan_dis_fake(y_dis - fake_mean))/2
+
+        discriminator.cleargrads()
+        loss_dis.backward()
+        dis_opt.update()
+
+        generator.cleargrads()
+        loss_gen.backward()
+        gen_opt.update()
+
+        sum_dis_loss += loss_dis.data.get()
+        sum_gen_loss += loss_gen.data.get()
+
+        if epoch%interval==0 and batch ==0:
+            serializers.save_npz('discriminator_getchu.model',discriminator)
+            serializers.save_npz('generator.model_getchu',generator)
+            pylab.rcParams['figure.figsize'] = (16.0,16.0)
+            pylab.clf()
+            z = zvis
+            z = Variable(z)
+            with chainer.using_config('train',False):
+                x = generator(z)
+            x = x.data.get()
+            for i_ in range(batchsize):
+                tmp = (np.clip((x[i_,:,:,:])*127.5 + 127.5, 0, 255)).transpose(1,2,0).astype(np.uint8)
+                pylab.subplot(10,10,i_+1)
+                pylab.imshow(tmp)
+                pylab.axis('off')
+                pylab.savefig('%svisualize_%d.png'%(image_dir, epoch))
+
+    print('epoch : {} dis_loss : {} gen_loss : {}'.format(epoch,sum_dis_loss/Ntrain,sum_gen_loss/Ntrain))
