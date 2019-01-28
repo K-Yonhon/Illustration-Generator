0733143a2d0093d4c46cd9acde9b56bac894feca
Lento <crosssceneofwindff@gmail.com>
2019-01-29 00:08:53 +0900

    on experiment

---
+++ StyleGAN/model.py
@@ -0,0 +1,219 @@
+import chainer
+import chainer.links as L
+import chainer.functions as F
+from chainer import cuda,Chain,initializers
+import numpy as np
+
+xp = cuda.cupy
+cuda.get_device(0).use()
+
+class MappingNetwork(Chain):
+    def __init__(self, base=512, layers=8):
+        w = initializers.Normal(0.02)
+        super(MappingNetwork, self).__init__()
+        mapping_net = chainer.ChainList()
+        for _ in range(layers):
+            mapping = L.Linear(base, base, initialW=w)
+            mapping_net.add_link(mapping)
+
+        with self.init_scope():
+            self.mapping_net = mapping_net
+
+    def __call__(self, x):
+        for m in self.mapping_net.children():
+            x = m(x)
+
+        return x
+
+class AffineTransform(Chain):
+    def __init__(self, ch_list = [512, 512, 512, 512, 256, 256, 128, 128, 64]):
+        cbrs = chainer.ChainList()
+        super(AffineTransform, self).__init__()
+        for i in range(1, len(ch_list)):
+            cbrs.add_link(CBR(ch_list[i-1], ch_list[i]))
+
+        with self.init_scope():
+            self.cbrs = cbrs
+
+    def __call__(self, x, stage):
+        x = x.reshape(x.shape[0], x.shape[1], 1, 1)
+        for i, cbr in enumerate(self.cbrs.children()):
+            x = F.unpooling_2d(x,2,2,0,cover_all=False)
+            x = cbr(x)
+            if i == stage + 1:
+                break
+
+        return x
+
+class ScaleFactors(Chain):
+    def __init__(self):
+        super(ScaleFactors, self).__init__()
+        pass
+
+    def __call__(self):
+        pass
+
+def calc_mean_std(feature, eps=1e-5):
+    batch, channels, _, _ = feature.shape
+    feature_a = feature.reshape(batch, channels, -1).data
+    feature_var = xp.var(feature_a,axis = 2) + eps
+    feature_var = chainer.as_variable(feature_var)
+    feature_std = F.sqrt(feature_var).reshape(batch, channels, 1,1)
+    feature_mean = F.mean(feature.reshape(batch, channels, -1), axis = 2)
+    feature_mean = feature_mean.reshape(batch, channels, 1,1)
+    
+    return feature_std, feature_mean
+
+def adain(content_feature, style_feature):
+    shape = content_feature.shape
+    style_std, style_mean = calc_mean_std(style_feature)
+    style_mean = F.broadcast_to(style_mean, shape = shape)
+    style_std = F.broadcast_to(style_std, shape = shape)
+    
+    content_std, content_mean = calc_mean_std(content_feature)
+    content_mean = F.broadcast_to(content_mean, shape = shape)
+    content_std = F.broadcast_to(content_std, shape = shape)
+    normalized_feat = (content_feature - content_mean) / content_std
+    
+    return normalized_feat * style_std + style_mean
+
+class CBR(Chain):
+    def __init__(self, in_channels, out_channels):
+        w = initializers.Normal(0.02)
+        super(CBR, self).__init__()
+        with self.init_scope():
+            self.c = L.Convolution2D(in_channels, out_channels, 3,1,1,initialW=w)
+            self.bn = L.BatchNormalization(out_channels)
+
+    def __call__(self, x):
+        h = F.leaky_relu(self.bn(self.c(x)))
+
+        return h
+
+class InitialBlock(Chain):
+    def __init__(self, in_channels, out_channels):
+        w = initializers.Normal(0.02)
+        super(InitialBlock, self).__init__()
+        with self.init_scope():
+            self.cbr0 = CBR(in_channels, out_channels)
+
+            self.b0 = ScaleFactors()
+            self.b1 = ScaleFactors()
+
+            self.a0 = AffineTransform()
+            self.a1 = AffineTransform()
+
+    def __call__(self, x, w, noise=None):
+        #scale = self.b0(noise) + x
+        scale = x
+        affine = self.a0(w, stage=0)
+        h = adain(scale, affine)
+        h = self.cbr0(h)
+
+        #scale = self.b1(noise) + h
+        scale = h
+        affine = self.a1(w, stage=0)
+        h = adain(scale, affine)
+
+        return h
+
+class Block(Chain):
+    def __init__(self, in_channels, out_channels):
+        w = initializers.Normal(0.02)
+        super(Block, self).__init__()
+        with self.init_scope():
+            self.cbr0 = CBR(in_channels, out_channels)
+            self.cbr1 = CBR(in_channels, out_channels)
+
+            self.b0 = ScaleFactors()
+            self.b1 = ScaleFactors()
+
+            self.a0 = AffineTransform()
+            self.a1 = AffineTransform()
+
+    def __call__(self, x, stage, w, noise=None):
+        h = F.unpooling_2d(x, 2,2,0,cover_all=False)
+        h = self.cbr0(h)
+
+        #scale = self.b0(noise) + h
+        scale = h
+        affine = self.a0(w, stage)
+        h = adain(scale, affine)
+        h = self.cbr0(h)
+
+        
+        #scale = self.b1(noise) + h
+        scale = h
+        affine = self.a1(w, stage)
+        h = adain(scale, affine)
+
+        return h
+
+class Generator(Chain):
+    def __init__(self, base=32, ch_list = [512, 256, 256, 128, 128, 64]):
+        w = initializers.Normal(0.02)
+        super(Generator, self).__init__()
+        blocks = chainer.ChainList()
+        outs = chainer.ChainList()
+        for i in range(1, len(ch_list)):
+            blocks.add_link(Block(ch_list[i-1], ch_list[i]))
+            outs.add_link(L.Convolution2D(ch_list[i-1], 3,3,1,1,initialW=w))
+        with self.init_scope():
+            self.b0 = InitialBlock(base*16, base*16)
+            self.blocks = blocks
+            self.outs = outs
+
+    def __call__(self, x, w, stage, noise=None):
+        h = self.b0(x, w)
+        for i, block in enumerate(self.blocks.children()):
+            h = block(h, stage, w)
+            if i == stage:
+                break
+
+        h = self.outs.children()[stage](h)
+
+        return h
+
+class CBR_Dis(Chain):
+    def __init__(self, in_channels, out_channels, down=True):
+        w = initializers.Normal(0.02)
+        self.down = down
+        super(CBR_Dis, self).__init__()
+        with self.init_scope():
+            self.cdown = L.Convolution2D(in_channels, out_channels, 4,2,1,initialW=w)
+            self.cpara = L.Convolution2D(in_channels, out_channels,3,1,1,initialW=w)
+
+            self.bnpara = L.BatchNormalization(out_channels)
+            self.bndown = L.BatchNormalization(out_channels)
+
+    def __call__(self, x):
+        if self.down:
+            h = F.relu(self.bndown(self.cdown(x)))
+
+        else:
+            h = F.relu(self.bnpara(self.cpara(x)))
+
+        return h
+
+class Discriminator(Chain):
+    def __init__(self, base=64, ch_list = [512, 256, 256, 128, 128, 64]):
+        super(Discriminator, self).__init__()
+        blocks = chainer.ChainList()
+        ins = chainer.ChainList()
+        for i in range(1, len(ch_list)):
+            blocks.add_link(CBR_Dis(ch_list[i], ch_list[i-1]))
+            ins.add_link(CBR_Dis(ch_list[i], ch_list[i-1], down=False))
+
+        with self.init_scope():
+            self.blocks = blocks
+            self.ins = ins
+            self.c5 = L.Linear(None, 1)
+
+    def __call__(self, x, stage):
+        h = self.ins.children()[stage](x)
+        for i, block in enumerate(self.blocks.children()):
+            h = block(h)
+            if i ==  stage:
+                break
+
+        return h
---
+++ StyleGAN/train.py
@@ -0,0 +1,143 @@
+import chainer
+import chainer.functions as F
+from chainer import cuda,Chain,optimizers,serializers
+import numpy as np
+import os
+from pathlib import Path
+import argparse
+import pylab
+import numpy as np
+import cv2 as cv
+from model import MappingNetwork, Generator, Discriminator
+
+xp = cuda.cupy
+cuda.get_device(0).use()
+
+def set_optimzier(model, alpha=0.0002, beta=0.5):
+    optimizer = optimizers.Adam(alpha=alpha, beta1=beta)
+    optimizer.setup(model)
+
+    return optimizer
+
+def prepare_dataset(filename, size = 128):
+    if filename.endswith(".png"):
+        image_orig = cv.imread(filename)
+        image_orig = cv.resize(image_orig,(size,size),interpolation=cv.INTER_CUBIC)
+        hflip = np.random.choice([True,False])
+        if hflip:
+            image_orig = image_orig[::-1,:,:]
+        image = image_orig[:,:,::-1]
+        image = image.transpose(2,0,1)
+        image = (image-127.5)/127.5
+        
+        return image
+
+parser = argparse.ArgumentParser(description="StyleGAN")
+parser.add_argument("--e", default=1000, type=int, help="the number of epochs")
+parser.add_argument("--b", default=64, type=int, help="batch size")
+parser.add_argument("--i", default=2000, type=int, help="the number of iterations")
+parser.add_argument("--lgp", default=10.0, type=float, help="the weight of gradient penalty")
+parser.add_argument("--stage", default=6, type=int, help="the number of stages")
+parser.add_argument("--si", default=300000, type=int, help="stage interval")
+parser.add_argument('--n', default=22000, type=int, help = "the number of train images")
+
+outdir = Path('./outdir')
+outdir.mkdir(parents=False, exist_ok=True)
+
+image_path = '/usr/MachineLearning/Dataset/face_getchu_2/'
+image_list = os.listdir(image_path)
+
+args = parser.parse_args()
+epochs = args.e
+batchsize = args.b
+iterations = args.i
+lambda_gp = args.lgp
+stages = args.stage
+stage_interval = args.si
+Ntrain = args.n
+
+mapping = MappingNetwork()
+mapping.to_gpu()
+map_opt = set_optimzier(mapping)
+
+generator = Generator()
+generator.to_gpu()
+gen_opt = set_optimzier(generator)
+
+discriminator = Discriminator()
+discriminator.to_gpu()
+dis_opt = set_optimzier(discriminator)
+
+const = np.random.uniform(-1, 1, size=(1, 512,4,4)).astype(np.float32)
+const = np.tile(const, (batchsize,1,1,1))
+np.save('./const.npy', const)
+
+const = chainer.as_variable(cuda.to_gpu(const))
+test_latent = chainer.as_variable(xp.random.uniform(-1, 1, size=(batchsize, 512)).astype(xp.float32))
+
+counter = 0
+
+for epoch in range(epochs):
+    sum_dis_loss = 0
+    sum_gen_loss = 0
+    for batch in range(0, Ntrain, batchsize):
+        image_box = []
+        for _ in range(batchsize):
+            rnd = np.random.randint(Ntrain)
+            image = prepare_dataset(image_path + image_list[rnd])
+            image_box.append(image)
+
+        x = chainer.as_variable(xp.array(image_box).astype(xp.float32))
+        z = chainer.as_variable(xp.random.uniform(-1,1,size=(batchsize, 512)).astype(xp.float32))
+        stage = counter / stage_interval
+
+        m = mapping(z)
+        noise = chainer.as_variable(xp.random.uniform(-1, 1, size=(batchsize, 512, 4, 4)).astype(xp.float32))
+
+        y = generator(const, m, stage)
+        y_dis = discriminator(y, stage)
+        x_dis = discriminator(x, stage)
+
+        dis_loss = F.mean(F.softplus(-x_dis)) + F.mean(F.softplus(y_dis))
+
+        eps = xp.random.uniform(0,1,size = batchsize).astype(xp.float32)[:,None,None,None]
+        x_mid = eps * y + (1.0 - eps) * x
+
+        y_mid = F.sum(discriminator(x_mid, stage))
+        grad,  = chainer.grad([y_mid], [x_mid], enable_double_backprop=True)
+        grad = F.sqrt(F.sum(grad*grad, axis=(1,2,3)))
+        loss_gp = lambda_gp * F.mean_squared_error(grad, xp.ones_like(grad.data))
+
+        dis_loss += loss_gp
+
+        discriminator.cleargrads()
+        dis_loss.backward()
+        dis_opt.update()
+        dis_loss.unchain_backward()
+
+        z = chainer.as_variable(xp.uniform(-1,1,size=(batchsize, 512)).astype(xp.float32))
+        m = mapping(z)
+        noise = xp.random.uniform(-1, 1, size=(batchsize, 512, 4, 4))
+
+        y = generator(z, m, noise, stage)
+        y_dis = discriminator(y, stage)
+
+        gen_loss = F.mean(F.softplus(-y_dis))
+
+        generator.cleargrads()
+        mapping.cleargrads()
+        gen_loss.backward()
+        gen_opt.update()
+        map_opt.update()
+        gen_loss.unchain_backward()
+
+        sum_dis_loss += dis_loss.data.get()
+        sum_gen_loss += gen_loss.data.get()
+
+        if batch == 0:
+            serializers.save_npz('generator.model', generator)
+            serializers.save_npz('mapping.model', mapping)
+
+    print('epoch : {}'.format(epoch))
+    print('discriminator loss : {}'.format(sum_dis_loss / iterations))
+    print('generator loss : {}'.format(sum_gen_loss / iterations))
